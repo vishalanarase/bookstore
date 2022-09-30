ceeed22a95e87e501956fc59a8e502517a282460
Vishal Anarse <vishalanarse11@gmail.com>
2022-09-30 21:57:18 +0530

    Add basic srver
    Signed-off-by: Vishal Anarse <vishalanarse11@gmail.com>

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
+)
 
 func main() {
 	fmt.Println("It's API")
+	engine := gin.New()
+
+	engine.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	err := engine.Run(":8080")
+	if err != nil {
+		log.Fatal().
+			Err(err).
+			Msg("Failed to start the gin server")
+	}
 }
