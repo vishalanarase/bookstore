12adca734adc64ff035c4ef0a52355d6d747b5f2
Vishal Anarase <iamvishalanarase@gmail.com>
2024-05-11 23:36:08 +0530

    Add mock test for Get and Create controller
    Signed-off-by: Vishal Anarase <iamvishalanarase@gmail.com>

--- api/controllers/book/books_test.go
+++ api/controllers/book/books_test.go
@@ -5,63 +5,120 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/gin-gonic/gin"
-	"github.com/stretchr/testify/assert"
+	. "github.com/onsi/gomega"
 	"github.com/stretchr/testify/mock"
 	"github.com/vishalanarase/bookstore/api/middleware"
 	"github.com/vishalanarase/bookstore/internal/data"
 	"github.com/vishalanarase/bookstore/internal/mocks"
 )
 
+type BookResponse struct {
+	code int
+	body data.Book
+}
+
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
 
 	os.Exit(m.Run())
 }
+func TestCreate(t *testing.T) {
+	g := NewWithT(t)
 
-// func TestList(t *testing.T) {
-// 	router := gin.Default()
-// 	router.GET("/v1/books", List)
-
-// 	rr := httptest.NewRecorder()
-// 	request, err := http.NewRequest(http.MethodGet, "/v1/books", nil)
-// 	assert.NoError(t, err)
-
-// 	router.ServeHTTP(rr, request)
-
-// 	assert.NoError(t, err)
-// 	assert.Equal(t, http.StatusOK, rr.Code)
-// }
-
-func TestGet(t *testing.T) {
-	mockBookResp := &data.Book{
-		ID:   "1",
-		Name: "Test",
+	tests := map[string]struct {
+		payload string
+		resp    BookResponse
+	}{
+		"should return 200": {
+			payload: `{"id":"11","name":"Test","rating":4}`,
+			resp: BookResponse{
+				code: http.StatusOK,
+				body: data.Book{
+					ID:     "11",
+					Name:   "Test",
+					Rating: 4,
+				},
+			},
+		},
 	}
 
-	mm := mocks.NewMockModels()
-	mockBook := new(mocks.MockBook)
-	mockBook.On("GetBook", mock.AnythingOfType("*gin.Context"), "1").Return(mockBookResp, nil)
-	mm.Book = mockBook
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			mm := mocks.NewMockModels()
+			mockBook := new(mocks.MockBook)
+			mm.Book = mockBook
 
-	rr := httptest.NewRecorder()
-	request, err := http.NewRequest(http.MethodGet, "/v1/books/1", nil)
-	assert.NoError(t, err)
+			mockBook.On("Create", mock.AnythingOfType("*gin.Context"), test.resp.body).Return(test.resp.body, nil)
 
-	router := gin.Default()
-	router.Use(middleware.Models(*mm))
-	router.GET("/v1/books/:id", Get)
+			rr := httptest.NewRecorder()
+			request, err := http.NewRequest(http.MethodPost, "/v1/books", strings.NewReader(test.payload))
+			g.Expect(err).To(BeNil())
 
-	router.ServeHTTP(rr, request)
+			router := gin.Default()
+			router.Use(middleware.Models(*mm))
+			router.POST("/v1/books", Create)
+			router.ServeHTTP(rr, request)
 
-	assert.Equal(t, http.StatusOK, rr.Code)
+			g.Expect(test.resp.code).To(Equal(rr.Code))
 
-	respBody := data.Book{}
-	err = json.Unmarshal(rr.Body.Bytes(), &respBody)
-	assert.NoError(t, err)
+			respBody := data.Book{}
+			err = json.Unmarshal(rr.Body.Bytes(), &respBody)
+			g.Expect(err).To(BeNil())
 
-	assert.Equal(t, mockBookResp, &respBody)
-	mockBook.AssertExpectations(t)
+			g.Expect(test.resp.body).To(Equal(respBody))
+		})
+	}
+}
+
+func TestGet(t *testing.T) {
+	g := NewWithT(t)
+
+	tests := map[string]struct {
+		payload string
+		resp    BookResponse
+	}{
+		"should return 200": {
+			payload: "1",
+			resp: BookResponse{
+				code: http.StatusOK,
+				body: data.Book{
+					ID:     "1",
+					Name:   "Test",
+					Rating: 4,
+				},
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			mm := mocks.NewMockModels()
+			mockBook := new(mocks.MockBook)
+			mm.Book = mockBook
+
+			mockBook.On("Get", mock.AnythingOfType("*gin.Context"), test.payload).Return(test.resp.body, nil)
+
+			rr := httptest.NewRecorder()
+			request, err := http.NewRequest(http.MethodGet, "/v1/books/"+test.payload, nil)
+			g.Expect(err).To(BeNil())
+
+			router := gin.Default()
+			router.Use(middleware.Models(*mm))
+			router.GET("/v1/books/:id", Get)
+			router.ServeHTTP(rr, request)
+
+			g.Expect(test.resp.code).To(Equal(rr.Code))
+
+			respBody := data.Book{}
+			err = json.Unmarshal(rr.Body.Bytes(), &respBody)
+			g.Expect(err).To(BeNil())
+
+			g.Expect(test.resp.body).To(Equal(respBody))
+
+		})
+	}
 }
--- internal/mocks/book.go
+++ internal/mocks/book.go
@@ -21,24 +21,12 @@ func (m *MockBook) List(ctx *gin.Context) ([]data.Book, error) {
 }
 
 func (m *MockBook) Get(ctx *gin.Context, uuid string) (data.Book, error) {
-	ret := m.Called(ctx, uuid)
+	args := m.Called(ctx, uuid)
+	return args.Get(0).(data.Book), args.Error(1)
 
-	// first value passed to "Return"
-	var r0 *data.Book
-	if ret.Get(0) != nil {
-		// we can just return this if we know we won't be passing function to "Return"
-		r0 = ret.Get(0).(*data.Book)
-	}
-
-	var r1 error
-
-	if ret.Get(1) != nil {
-		r1 = ret.Get(1).(error)
-	}
-
-	return *r0, r1
 }
 
 func (m *MockBook) Create(ctx *gin.Context, book data.Book) (data.Book, error) {
-	return data.Book{}, nil
+	args := m.Called(ctx, book)
+	return args.Get(0).(data.Book), args.Error(1)
 }
