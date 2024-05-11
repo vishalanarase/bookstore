8619fbd17797304cffcea98c6e65e368df90d386
Vishal Anarase <iamvishalanarase@gmail.com>
2024-05-11 23:36:07 +0530

    Add controller for ping
    Signed-off-by: Vishal Anarase <iamvishalanarase@gmail.com>

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,21 +2,17 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog/log"
+	"github.com/vishalanarase/bookstore/app/api/controllers"
 )
 
 func main() {
 	fmt.Println("It's API")
 	engine := gin.New()
 
-	engine.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	controllers.AddRoutes(engine)
 
 	err := engine.Run(":8080")
 	if err != nil {
