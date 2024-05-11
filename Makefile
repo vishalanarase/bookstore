cd1f088256c1f6ad46b3b053846daeb9ffc47d8f
Vishal Anarase <iamvishalanarase@gmail.com>
2024-05-11 23:36:07 +0530

    Add db migration support
    Signed-off-by: Vishal Anarase <iamvishalanarase@gmail.com>

--- Makefile
+++ Makefile
@@ -1,3 +1,9 @@
+.PHONY: migrations
 
-run:
+migrations:
+	cd $(PWD)/db && \
+	soda -c config/database.yaml create && \
+	soda -c config/database.yaml migrate up
+
+run: migrations
 	go run cmd/api/main.go
---
+++ db/migrations/20221001171405_create_initial_dumps.mysql.up.sql
@@ -0,0 +1,17 @@
+
+DROP TABLE IF EXISTS `books`;
+
+CREATE TABLE `books` (
+  `id` VARCHAR(40) NOT NULL,
+  `title` VARCHAR(50) NOT NULL,
+  `authorname` VARCHAR(30) NOT NULL,
+  `rating` INT,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `deleted_at` datetime DEFAULT NULL,
+  CONSTRAINT rating_1_to_10 CHECK (rating IS NULL OR
+    (`rating` >= 1 and `rating` <= 10)),
+  PRIMARY KEY (`id`), 
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `title` (`id`)
+);
