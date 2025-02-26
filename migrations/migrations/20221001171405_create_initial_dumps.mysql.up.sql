e0e2949b579b64196a5e2282723e4344d60827f0
Vishal Anarase <iamvishalanarase@gmail.com>
2025-02-26 13:57:30 +0530

    Remove ratings from book
    Signed-off-by: Vishal Anarase <iamvishalanarase@gmail.com>

--- migrations/migrations/20221001171405_create_initial_dumps.mysql.up.sql
+++ migrations/migrations/20221001171405_create_initial_dumps.mysql.up.sql
@@ -9,11 +9,9 @@ CREATE TABLE `books` (
   `isbn` VARCHAR(13) NOT NULL,
   `year` INT NOT NULL,
   `edition` INT,
-  `rating` TINYINT NOT NULL,
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `deleted_at` DATETIME DEFAULT NULL,
-  CONSTRAINT rating_1_to_10 CHECK (rating >= 1 AND rating <= 10),
   PRIMARY KEY (`id`),
   UNIQUE KEY `isbn` (`isbn`), 
   UNIQUE KEY `title` (`title`)
